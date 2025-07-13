19b0c48a206fa2829e8b6f940160db60ea399625
Benny Attias <bennyattias305@gmail.com>
2025-07-13 09:19:53 -0400

    FINAL WORKING VERSION (7 hours to reach 10k emails) [Main issues were the lock and one of the exceptins was not being handled with a "continue;" clause.]

--- src/main/java/EmailData.java
+++ src/main/java/EmailData.java
@@ -1,5 +1,29 @@
-import java.sql.Timestamp;
+import crawlercommons.robots.BaseRobotRules;
+import crawlercommons.robots.SimpleRobotRulesParser;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.validator.routines.UrlValidator;
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.UnknownHostException;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class EmailData {
@@ -32,3 +56,9 @@ public class EmailData {
         return timestamp;
     }
 }
+
+
+
+
+
+
--- src/main/java/WebScraper.java
+++ src/main/java/WebScraper.java
@@ -27,22 +27,21 @@ Pseudocode:
     - Use jsoup
 2- Pull out all emails on touro.edu and all links to be searched for email addresses
     - Use crawler commons library to check if robots.txt allows scraping the link
-    - use jsoup to fetch the email html notation
-    - use absUrl() for relative urls's
     - implement error/exception handling logic
     - Add rate limiting
-    - use regex to extract only the email addresses (group 1): <a\s+href=\"mailto:(\w+@(\w+\.)+?(com|edu|net|org|))\">.*</a>
+    - use regex to extract only the email addresses
 3- Create a set for the emails to prevent duplicates
 4- Canonize the email addresses to be fully capitalized
     - toUpperCase()
 5- Add the canonized emails to the set
 6- Create a set for the links to prevent duplicates
 7- Create a queue for the links to access them in a BFS-consistent way
-8- Add the links on touro.edu to the set
-9- If the linkSet.add() method returns true, that link has not yet been scraped so it should be added to the queue
+8- Add the links on the website to the set
+9- If the linkSet.add() method returns true, that link has not yet been added so it should be added to the queue
 10- Go through each link in the queue and repeat steps 2-9 (except for creating new sets and queues)
-11- Steps 2-9 should be looped in a while loop that continues until the emailSet has a size of 10,000
-12- Once the while loop ends, connect to the database and bulk add the emails to the database
+11- Once the emails reach a certain amount, connect to the database and bulk add the emails to the database
+12- Steps 2-9 should be looped in a while loop that continues until the emailSet has a size of 10,000
+
  */
 public class WebScraper implements Runnable {
     private static volatile Queue<String> linkQueue = new ConcurrentLinkedQueue<String>();
@@ -57,11 +56,15 @@ public class WebScraper implements Runnable {
     private URLConnection connection = null;
     private byte[] content = null;
     private Document doc = null; //This won't work on multi threading
-    private static volatile Logger logger = LoggerFactory.getLogger(WebScraper.class);
+    public static volatile Logger logger = LoggerFactory.getLogger(WebScraper.class);
     private static volatile AtomicInteger emailCount = new AtomicInteger(0);
     private static volatile Map<String, String> env = Collections.synchronizedMap(new HashMap<>());
-    private static volatile Queue<EmailData> emailDataQueue = new ConcurrentLinkedQueue<EmailData>();
-    private static final Object lock = new Object();
+    public static volatile Queue<EmailData> emailDataQueue = new ConcurrentLinkedQueue<EmailData>();
+    public static final Object lock = new Object();
+    public static volatile AtomicInteger emailsStored = new AtomicInteger(0);
+    public static volatile boolean isThreadInLock = false;
+    private static volatile boolean islinkQueueAlmostEmpty = false;
+
 
 
 
@@ -83,12 +86,15 @@ public class WebScraper implements Runnable {
 
     public void run() {
 
-        while (emailSet.size() < 10000) {
+        while (emailsStored.get() < 10000) {
+// Check if we have accumulated enough emails for a bulk insert into the database
+            int bulkQuantity = 500;
+            if (emailDataQueue.size() >= bulkQuantity && !isThreadInLock) {
+                logger.info(Thread.currentThread().getName() + ": entered DB section, outside lock");
 
-            // Check if we have enough emails to insert into database
-            synchronized (lock) {
-                int bulkQuantity = 500;
-                if (emailDataQueue.size() >= bulkQuantity) {
+                synchronized (lock) {
+                    isThreadInLock = true; //when this is true, the other threads won't even try entering the lock
+                    logger.info(Thread.currentThread().getName() + ": entered DB section, inside lock");
 
                     Map<String, String> env = System.getenv();
                     String endpoint = env.get("db_connection");
@@ -104,7 +110,6 @@ public class WebScraper implements Runnable {
                                     + "loginTimeout=30;";
 
                     try (Connection connection = DriverManager.getConnection(connectionUrl)) {
-                        // Multi-row insert - Much more efficient
                         String sql = "INSERT INTO Emails (EmailID, Email, Source, TimeStamp) VALUES (?, ?, ?, ?)";
                         for (int i = 0; i < bulkQuantity - 1; i++) {
                             sql = sql + ", (?, ?, ?, ?)";
@@ -112,45 +117,49 @@ public class WebScraper implements Runnable {
                         sql = sql + ";";
 
                         try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-                            // Set all parameters in one go
-                            int counter = 0;
                             for (int i = 0; i < bulkQuantity; i++) {
                                 EmailData emailData = emailDataQueue.poll();
+                                emailsStored.incrementAndGet();
                                 int index = i * 4 + 1;
                                 stmt.setString(index, String.valueOf(emailData.getEmailId()));
                                 stmt.setString(index + 1, String.valueOf(emailData.getEmail()));
                                 stmt.setString(index + 2, String.valueOf(emailData.getSource()));
                                 stmt.setString(index + 3, String.valueOf(emailData.getTimestamp()));
                             }
-//                            logger.info("Executing thread: " + Thread.currentThread().getName());
-                            // And so on...
                             stmt.execute();
+                            logger.info("Executing thread: " + Thread.currentThread().getName() + " Stored into DB");
                         }
 
                     } catch (SQLException e) {
                         e.printStackTrace();
                     }
-                }
+                } isThreadInLock = false; //once thread leaves synchronized section, so that other threads can enter when needed
             }
 
-
-
             String link;
+                if (linkQueue.size() <= 300) {
+                    islinkQueueAlmostEmpty = true;
+                } else if (linkQueue.size() >= 700) {
+                    islinkQueueAlmostEmpty = false;
+                }
+
+                //To avoid getting an exception from the link being null when inserted into jsoup.connect()
             while (true) {
                 link = linkQueue.poll();
                 if (link != null) {
                     break;
                 }
-                try {
-                    TimeUnit.SECONDS.sleep(1);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
+//                try {
+//                    //To have the scraper take a one-second pause in between requests
+//                    TimeUnit.SECONDS.sleep(1);
+//                } catch (InterruptedException e) {
+//                    throw new RuntimeException(e);
+//                }
             }
 
             try {
+                Thread.sleep(3000);
                 this.doc = Jsoup.connect(link)
-                        //.ignoreHttpErrors(true)
                         .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.6998.166 Safari/537.36")
                         .get();
             } catch (IllegalArgumentException e) {
@@ -162,22 +171,24 @@ public class WebScraper implements Runnable {
             } catch (HttpStatusException e) {
                 e.printStackTrace();
                 continue;
-            } catch (IOException e) {
+            } catch (IOException | InterruptedException e) {
                 e.printStackTrace();
                 continue;
             }
-
             Elements html = doc.getElementsByTag("a");
-            try {
-                scrapeLinks(html);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+            //Will only scrape new links if the linkqueue contains under 300 links and continues scraping until it surpasses 700 links
+            if (islinkQueueAlmostEmpty) {
+                try {
+                    scrapeLinks(html);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    continue;
+                }
             }
+
             html = doc.getAllElements();
             scrapeEmails(html, link);
-//            System.out.println(doc.title());
-//            System.out.println("emailSet:" + emailSet);
-            System.out.println("emailset size:" + emailSet.size());
+            System.out.println("linkset size: " + linkSet.size() + ", linkqueue size: " + linkQueue.size());
         }
     }
 
@@ -190,14 +201,10 @@ public class WebScraper implements Runnable {
      */
 
     public void scrapeEmails(Elements html, String link) {
-//        logger.info("from scrapeEmails method: scraping emails of " + link);
         String htmlString = html.toString();
         Pattern pattern = Pattern.compile("(?i)[A-Z0-9._%+-]+@[A-Z0-9\\.-_]+\\.[A-Z]{2,10}"); //TODO get a better regex from very good developers on stackoverflow
         Matcher matcher = pattern.matcher(htmlString);
-        int emailsFound = 0;
-//        logger.info("before while loop");
         while (matcher.find()) {
-//            logger.info("inside while loop");
             String email = matcher.group().toUpperCase();
             if(email.endsWith(".PNG") || email.endsWith(".JPG") || email.endsWith(".JPEG")
                     || email.endsWith(".GIF") || email.endsWith(".PDF") || email.endsWith(".WEBP")
@@ -207,19 +214,12 @@ public class WebScraper implements Runnable {
                     || email.endsWith(".XLS")) {
                 continue;
             }
-//            logger.info("inside while loop after to upper case");//(?!.*(\.png|\.jpg|\.gif|\.pdf).*)
             if (emailSet.add(email)){
-                emailsFound++;
                 int currentEmailId = emailCount.incrementAndGet();
                 logger.info("emailID: " + currentEmailId + " email: " + email + " from: " + link);
                 emailDataQueue.add(new EmailData(currentEmailId, email, link, Timestamp.valueOf(LocalDateTime.now())));
-//                logger.info("Added to queue. Queue size now: " + emailDataQueue.size());
             }
         }
-//        if (emailsFound > 0) {
-//            logger.info("Found " + emailsFound + " emails on " + link);
-//        }
-//        logger.info("after while loop");
     }
 
     /**
@@ -231,13 +231,14 @@ public class WebScraper implements Runnable {
 
     public void scrapeLinks(Elements html) throws IOException {
         String htmlString = html.toString();
-        Pattern pattern = Pattern.compile("(?i)(?!.*(\\.png|\\.jpg|\\.gif|\\.pdf|twitter|vimeo|x\\.com|facebook|\\.gov).*)https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()!@:%_\\+.~#?&\\/\\/=]*)");
+        Pattern pattern = Pattern.compile("(?i)(?!.*(\\.png|\\.jpg|\\.gif|\\.pdf|\\.webp|twitter|linkedin|vimeo|x\\.com|facebook|\\.gov).*)https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()!@:%_\\+.~#?&\\/\\/=]*)");
         Matcher matcher = pattern.matcher(htmlString);
         while (matcher.find()) {
             String link = matcher.group(0).toUpperCase();
             if (isScrapeAllowed(link) && linkSet.add(link)) {
                 linkQueue.add(link);
             }
+
         }
     }
 
@@ -254,13 +255,10 @@ public class WebScraper implements Runnable {
         if (matcher.find()){
             url = "https://" + matcher.group() + "/robots.txt";
         }
-//        logger.info("link: " + link);
         if (!isValidURL(link)){
-//            logger.info("link: " + link + " is not a valid URL");
             return false;
         }
         if (!robotsTxtSet.add(url)){
-//            logger.info("robotsTxtSet: " + url + " already exists");
             return robotsTxtMap.get(url);
         }
 
@@ -268,28 +266,23 @@ public class WebScraper implements Runnable {
             u = new URL(url);
         } catch (MalformedURLException e) {
             robotsTxtMap.put(url, false);
-//            logger.info("isAllowed: " + false);
             return false;
         }
         try {
             connection = u.openConnection();
         } catch (IOException e) {
             robotsTxtMap.put(url, false);
-//            logger.info("isAllowed: " + false);
             return false;
         }
-//        logger.info("connection: " + connection);
         try {
             content = IOUtils.toByteArray(connection);
         } catch (IOException e) {
             robotsTxtMap.put(url, false);
-//            logger.info("isAllowed: " + false);
             return false;
-        } //emailSet:[%20MARKETING@HIRINGTHING.COM, PRIVACY@IMPERVA.COM, CANGRADE@4X.PNG, HT-ATS-AND-OB-ILLUSTRATION-HOMEPAGE@2X-1024X893.PNG, SUPPORT@HIRINGTHING.COM, HT-ATS-ILLUSTRATION-BOTH-HEX-FOR-LIGHT@2X-1024X685.PNG, 3BBE57A973254129BCB93E47DC0CC46F@O343074.INGEST.SENTRY.IO, INFO@HRLOGICS.COM, HIRINGTHING-LOGO-BLUE@2X.PNG, CUSTOMERSERVICE@ADVANCED-ONLINE.COM, MARKETING@HIRINGTHING.COM, INFO@JOBMA.COM]
+        }
         rules = parser.parseContent(url, content, "text/plain", agentNames);
         boolean isAllowed = rules.isAllowed(url);
         robotsTxtMap.put(url, isAllowed);
-//            logger.info("isAllowed: " + isAllowed);
         return isAllowed;
     }
 
@@ -300,7 +293,6 @@ public class WebScraper implements Runnable {
                 || link.contains("TWITTER")
                 || link.contains("X.COM")
                 || link.contains("SHOPIFY")) {
-//            logger.info("404 Not Found");
             return false;
         }
         return validator.isValid(link);
